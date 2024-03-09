e154d34fd2299069404efa046ac0669535f48892
zand-yasin <zand.kurdy@gmail.com>
2024-03-10 00:30:17 +0300

    send the event back from event but to posts and comments

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,11 +12,11 @@ app.get("/events", (req, res) => {
 });
 
 app.post("/events", (req, res) => {
-  const { event } = req.body;
+  const event = req.body;
 
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  // axios.post("http://localhost:4002/events", event);
 
   res.send({ status: "OK" });
 });
