afa6cc1c16aac27e7b1719bbb2c84615a513e0d6
zand-yasin <zand.kurdy@gmail.com>
2024-03-10 22:03:27 +0300

    add query server

---
+++ query/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Recieved Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
